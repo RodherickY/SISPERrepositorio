60dd0897838d5833a6425dfc51fe68a8a54fbd1b
RodherickY <rodherick1505@gmail.com>
2024-12-04 12:18:43 -0500

    cambie de carpeta
    cambio de carpeta

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>SISPER</title>
+</head>
+<body>
+
+
+<center>
+<img src="img/Logo_UNJFSC.png" width="180" height="180">
+<br>
+<h1>PROCESO DE VALIDACIÓN ELECTRÓNICA PROYECTO EDUCATIVO REGIONAL</h1>
+<br><br>
+
+<form action="p_login.php" method="post">
+
+<fieldset id="grupito">
+    <center>
+            <input type="text" name="txtusuario" id="txt" autocomplete="off" maxlength="8">
+            <input type="password" name="txtpass" id="pwd" autocomplete="off" maxlength="8">
+            <input type="submit" value="Iniciar Sesion" id="btn">
+
+    </center>
+
+
+</fieldset>
+
+
+
+</form>
+
+
+</center>
+    
+</body>
+</html>
