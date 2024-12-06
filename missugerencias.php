dbe0be241a95e2f3cb0196553396a9bd3f5f9bf9
RodherickY <rodherick1505@gmail.com>
2024-12-06 18:16:26 -0500

    CON PAGINACION

--- missugerencias.php
+++ missugerencias.php
@@ -4,19 +4,25 @@ include("cabecera.php");
 
 $cod = $_SESSION["usuario"];
 
+// Determinar el límite inicial para la consulta según la paginación
+$cantidad = 5; // Cantidad de elementos por página
+$limite = isset($_GET["valor"]) ? $_GET["valor"] : 0;
+
 // Consulta para obtener sugerencias relacionadas con la persona o institución
 $sql = "SELECT s.*
         FROM SUGERENCIA s 
         INNER JOIN PERSONA p 
         ON s.codigo = p.codigo
         WHERE p.codigo = '$cod'
-        AND s.estado = 1";
-
+        AND s.estado = 1
+        LIMIT $limite, $cantidad";
 $query = mysqli_query($cn, $sql);
+
 ?>
 
 <br>
-
+<h2><center>SUGERENCIAS REALIZADAS</center></h2>
+<br>
 <div>
     <center>
         <table border="1" cellspacing="0" align="center" bgcolor="white" width="1000">
@@ -26,7 +32,6 @@ $query = mysqli_query($cn, $sql);
                 <th>ITEM</th>
                 <th>FECHA Y HORA</th>
                 <th>ACCIONES</th>
-
             </tr>
             <?php while ($row = mysqli_fetch_assoc($query)) { ?>
                 <tr>
@@ -34,8 +39,6 @@ $query = mysqli_query($cn, $sql);
                     <td><?php echo $row['tipo']; ?></td>
                     <td><?php echo $row['item']; ?></td>
                     <td><?php echo $row['fecha']; ?></td>
-                    
-                    
                     <td style="width: 20%;">
                         <center>
                             <a href="editarsugerencia.php?id=<?php echo $row["idsugerencia"] ?>" target="_parent" style="width: 20px; height:20px">
@@ -44,13 +47,22 @@ $query = mysqli_query($cn, $sql);
                             <a href="p_eliminarsugerencia.php?id=<?php echo $row["idsugerencia"] ?>" target="_parent" style="width: 15px; height:15px">
                                 <img src="img/eliminar.png" class="icono" style="width: 15%; height:15%">
                             </a>
-
                         </center>    
-
-
                     </td>
                 </tr>
             <?php } ?>
         </table>
     </center>
 </div>
+
+<br>
+
+<!-- Paginación -->
+<center>
+    <?php for ($i = 0; $i < 3; $i++) { 
+        $parametro = $i * $cantidad; ?>
+        <a target="_parent" href="missugerencias.php?valor=<?php echo $parametro; ?>">
+            <?php echo $i + 1; ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </a>
+    <?php } ?>
+</center>
